73b778b7fb3441511de1c5f4348f20dc042120a4
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-11 12:39:53 -0500

    add info to readme file and add beginning code to server.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mongodb_scraper
+
+## Overview:
+
+An application for scraping news from a particular website and adding it to the homepage. The user will also have the ability to save articles and save notes on specific articles.
+
+## App on Heroku
+
+## Author:
+ Amy Haerr
+
+
+## Images
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3030;
+
+// Initialize Express
+var app = express();
