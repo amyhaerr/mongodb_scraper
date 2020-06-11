3745553caa1945d51cd7119cd8a4779fc4f89e86
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-11 14:56:37 -0500

    work on handlebars

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // require dependencies
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 // set up port 
 var PORT = process.env.PORT || 3030;
@@ -13,8 +16,32 @@ var router = express.Router();
 // Designate public folder as static directory
 app.use(express.static(__dirname + "/public"));
 
+// connect to handlebars
+app.engine("handlebars", expressHandlebars ({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Body-parser for the app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 // Middleware
 app.use(router);
+
+// If deployed, use databas
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect mongoose to database
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection succeeded");
+    }
+});
 
 // Port for listening
 app.listen(PORT, function() {
