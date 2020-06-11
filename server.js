70e395338bacc32c717fb191dbb66f25c5d65f81
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-11 14:08:43 -0500

    work on server.js file

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// require dependencies
 var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+// set up port 
+var PORT = process.env.PORT || 3030;
 
-// Require all models
-var db = require("./models");
-
-var PORT = 3030;
-
-// Initialize Express
+// start our express app
 var app = express();
+
+// set up router for express
+var router = express.Router();
+
+// Designate public folder as static directory
+app.use(express.static(__dirname + "/public"));
+
+// Middleware
+app.use(router);
+
+// Port for listening
+app.listen(PORT, function() {
+    console.log("App is listening on port:" + PORT);
+});
