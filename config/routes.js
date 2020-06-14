6d74174a32b56ffaddc146a5101f536d79e02e12
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-14 12:15:44 -0500

    finish controllers

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,77 @@
-module.exports = function(router) {
-    router.get("/", function (req,res) {
-        res.render("home");
-    });
+var scrape = require("../scripts/scrape");
 
-    router.get("/saved", function(req, res) {
-        res.render("saved");
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+const notes = require("../controllers/notes");
+
+module.exports = function (router) {
+  router.get("/", function (req, res) {
+    res.render("home");
+  });
+
+  router.get("/saved", function (req, res) {
+    res.render("saved");
+  });
+
+  router.get("/api/fetch", function (req, res) {
+    headlinesController.fetch(function (err, docs) {
+      if (!docs || docs.insertedCount === 0) {
+        res.json({
+          message:
+            "There are no new stories today. Please check again another day!",
+        });
+      } else {
+        res.json({
+          message: "Added " + docs.insertedCount + " new stories",
+        });
+      }
     });
+  });
+
+ router.get("/api/headlines", function(req, res) {
+     var query = {};
+     if (req.query.saved) {
+         query = req.query;
+     }
+     headlinesController.get(query, function(data) {
+         res.json(data);
+     });
+ });
+  router.delete("/api/headlines/:id", function(req, res){
+      var query = {};
+      query._id = req.params.id;
+      headlinesController.delete(query, function(err, data) {
+          res.json(data);
+      });
+  });
+
+  router.patch("/api/headlines", function(req, res) {
+      headlinesController.update(req.body, function(err, data) {
+          res.json(data);
+      });
+  });
+
+  router.get("/api/notes/:headline_id?", function(req, res){
+      var query = {};
+      if (req.params.headline_id) {
+          query._id = req.params.headline_id;
+      }
+      notesController.get(query, function(err, data) {
+          res.json(data);
+      });
+  });
+
+  router.delete("/api/notes/:id", function(req, res) {
+      var query = {};
+      query._id = req.params.id;
+      notesController.delete(query, function(err, data){
+          res.json(data);
+      });
+  });
+
+  router.post("/api/notes", function(req, res){
+      notesController.save(req.body, function(data){
+          res.json(data);
+      });
+  });
 }
